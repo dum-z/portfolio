311f6d0eae3a6718166e7efc097187de16c209e5
Ronald Bancifra Nueva <83579997+dum-z@users.noreply.github.com>
2023-03-18 16:40:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Time In/Out</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Time In/Out</h1>
+      <form>
+        <label for="time-in">Time In:</label>
+        <input type="time" id="time-in" name="time-in">
+
+        <label for="time-out">Time Out:</label>
+        <input type="time" id="time-out" name="time-out">
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </body>
+</html>
